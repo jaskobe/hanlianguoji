c184533e310b858de573839c8a7d2917b9e536b4
Jasko <jaskobe@126.com>
2016-05-30 23:56:01 +0800

    no message

--- src/js/common.js
+++ src/js/common.js
@@ -2,15 +2,15 @@
  * Created by Jasko on 5/9/16.
  */
 
-
-$(function(){
-
+$(document).ready(function(){
     var setting = {
         //初始化
         init : function(){
             this.setNav();
             this.setFull();
             this.vertical();
+            this.maxFull();
+            this.center();
         },
         //设置导航
         setNav : function(){
@@ -21,6 +21,12 @@ $(function(){
             var nav = side.find("li>a");
             nav.click(function(){
                 $(this).addClass("nav-active").parent().siblings("").find("a").removeClass("nav-active");
+            });
+            //mobile 事件
+            var btn = $(".nav-btn"),
+                list = $(".nav-side>ul");
+            btn.click(function(){
+                list.slideToggle(500);
             })
         },
         //设置全屏
@@ -34,17 +40,40 @@ $(function(){
             $(".v-center").each(function(){
                 var $this = $(this),
                     box = $this.parent();
-                $this.css("margin-top",(box.height() - $this.height())/2);
+                $this.css({
+                    "top":(box.height() - $this.height())/2
+                });
+            })
+        },
+        center : function(){
+            $(".b-center").each(function(){
+                var $this = $(this),
+                    box = $this.parent();
+                $this.css({
+                    "top":(box.height() - $this.height())/2,
+                    "left":(box.width() - $this.width())/2
+                });
+            })
+        },
+        //设置最大满屏
+        maxFull : function(){
+            $(".max-full").each(function(){
+                $(this).css("max-height",$(window).height()-60);
             })
         }
     };
 
     //默认执行
     setting.init();
-
     //改变窗口执行
     $(window).resize(function(){
         setting.init();
     });
+    //懒加载
+    $("img.lazy").lazyload({
+        effect:'fadeIn',
+        threshold: 10, // 提前开始加载距离
+        failurelimit : 2 // 提前加载图片张数
 
+    });
 });
